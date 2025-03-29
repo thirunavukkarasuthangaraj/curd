7d1cd3e598032f4ee67ba33881d0545129d87e2f
thiru <thiru.t@mavens-i.com>
2025-03-30 05:25:23 +0530

    url adde

--- angular-frontend/src/app/employee.service.ts
+++ angular-frontend/src/app/employee.service.ts
@@ -8,7 +8,7 @@ import { Employee } from './employee';
 })
 export class EmployeeService {
 
-  private baseURL = "http://54.152.165.191:8080/api/v1/employees";
+  private baseURL = "http://3.89.217.136:8080/api/v1/employees";
 
   constructor(private httpClient: HttpClient) { }
 
