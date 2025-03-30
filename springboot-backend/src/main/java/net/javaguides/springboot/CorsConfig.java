3c742aa0c6fe97cf0e119ae289761ff7e1294a97
thiru <thiru.t@mavens-i.com>
2025-03-30 20:14:04 +0530

    cross

--- springboot-backend/src/main/java/net/javaguides/springboot/CorsConfig.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/CorsConfig.java
@@ -1,26 +1,16 @@
 package net.javaguides.springboot;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
- 
-
+  
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:4200","https://thiruangular.s3.us-east-1.amazonaws.com", "http://54.152.165.191") // Add your frontend URLs
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
+
 
 
