25fa946715d823151e24d39019fbc51e6084bdd7
thiru <thiru.t@mavens-i.com>
2025-03-31 14:28:32 +0530

    pipeline test

---
+++ springboot-backend-lampda/src/main/java/com/example/lambda/config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.backend.lambda.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public String exampleBean() {
+        return "Example Bean";
+    }
+}
