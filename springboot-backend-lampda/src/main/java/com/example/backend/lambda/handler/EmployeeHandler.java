4f3ae0550c8c0fbee4ba076c8f945b1b6e09b05c
thiru <thiru.t@mavens-i.com>
2025-03-31 23:53:05 +0530

    aws test

--- springboot-backend-lampda/src/main/java/com/example/backend/lambda/handler/EmployeeHandler.java
+++ springboot-backend-lampda/src/main/java/com/example/backend/lambda/handler/EmployeeHandler.java
@@ -3,10 +3,24 @@ package com.example.backend.lambda.handler;
  
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
- 
-public class EmployeeHandler implements RequestHandler<String, String> {
+import java.util.Map;
+
+public class EmployeeHandler implements RequestHandler<Map<String, String>, String> {
     @Override
-    public String handleRequest(String input, Context context) {
-        return "Hello, " + input + "! This is from AWS Lambda.";
+    public String handleRequest(Map<String, String> input, Context context) {
+        System.out.println("Received input: " + input); // Log the input to CloudWatch
+
+        // Extract data
+        String firstName = input.get("firstName");
+        String lastName = input.get("lastName");
+        String emailId = input.get("emailId");
+
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Email ID: " + emailId);
+
+        return "Employee details received: " + firstName + " " + lastName + " (" + emailId + ")";
     }
 }
+
+
