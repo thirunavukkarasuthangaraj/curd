10b5cffdb88392084159890e2b7918bc32adc75f
thiru <thiru.t@mavens-i.com>
2025-04-13 22:28:12 +0530

    test the error

--- springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -20,7 +20,7 @@ import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 
 @RestController
-@RequestMapping("/v1/")
+@RequestMapping("/api/v1/")
 public class EmployeeController {
 
 	@Autowired
