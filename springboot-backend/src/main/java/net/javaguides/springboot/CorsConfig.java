97a1b763f061b6d44031ec57c1d1750100e32f14
thiru <thiru.t@mavens-i.com>
2025-04-13 17:49:01 +0530

    server url added

--- springboot-backend/src/main/java/net/javaguides/springboot/CorsConfig.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/CorsConfig.java
@@ -8,8 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*")
-        .allowedOrigins("http://thirunavukkarasuthangaraj.fun").allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/**").allowedOrigins("*").allowedOrigins("http://thirunavukkarasuthangaraj.fun").allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
 
