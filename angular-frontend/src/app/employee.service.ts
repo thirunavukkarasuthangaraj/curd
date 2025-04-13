acd21daa5b644758f6345a01fa5a24e943b87f01
thiru <thiru.t@mavens-i.com>
2025-04-13 18:20:27 +0530

    changes

--- angular-frontend/src/app/employee.service.ts
+++ angular-frontend/src/app/employee.service.ts
@@ -8,7 +8,7 @@ import { Employee } from './employee';
 })
 export class EmployeeService {
 
-  private baseURL = "http://3.208.115.49:8080/api/v1/employees";
+  private baseURL =  "http://3.208.115.49:8080/api/v1/employees";
 
   constructor(private httpClient: HttpClient) { }
 
--- springboot-backend/src/main/java/net/javaguides/springboot/CorsConfig.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedOrigins("http://thirunavukkarasuthangaraj.fun").allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/**").allowedOrigins("*").allowedOrigins("https://thirunavukkarasuthangaraj.fun").allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
 
