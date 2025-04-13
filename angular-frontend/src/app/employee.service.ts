d31a2eadd6890adc303e3cede0a6af8baa23d343
thiru <thiru.t@mavens-i.com>
2025-04-13 18:49:20 +0530

    revert the url

--- angular-frontend/src/app/employee.service.ts
+++ angular-frontend/src/app/employee.service.ts
@@ -8,8 +8,7 @@ import { Employee } from './employee';
 })
 export class EmployeeService {
 
-  private baseURL = "https://thirunavukkarasuthangaraj.fun/api/v1/employees";
-  //private baseURL = "http://localhost:8080/api/v1/employees";
+  private baseURL = "http://3.208.115.49:8080/api/v1/employees";
 
   constructor(private httpClient: HttpClient) { }
 
