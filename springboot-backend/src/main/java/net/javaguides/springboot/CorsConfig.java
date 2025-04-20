79a90c1d1eb3bec427c3d0302b9a9453c9354a91
thiru <thiru.t@mavens-i.com>
2025-04-20 12:34:34 +0530

    test build 403

--- springboot-backend/src/main/java/net/javaguides/springboot/CorsConfig.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/CorsConfig.java
@@ -8,9 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedOrigins("https://thirunavukkarasuthangaraj.fun").allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/**")
+                .allowedOrigins("*", "https://thirunavukkarasuthangaraj.fun")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(false);
     }
 }
+
 
 
 
